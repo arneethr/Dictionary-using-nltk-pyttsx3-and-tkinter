03afd4a25d8cd604c4bd3f0111835135ac3dc214
Arneeth <157263299+arneethr@users.noreply.github.com>
2024-09-04 19:33:54 +0530

    last push

--- Dictionary using nltk/Dictionary.py
+++ Dictionary using nltk/Dictionary.py
@@ -5,7 +5,7 @@ from tkinter import *
 nltk.download('wordnet')
 
 def speak(audio):
-    engine = pyttsx3.init(sapi5)
+    engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     voice = engine.setProperty('voice',voices[0].id)
     engine.say(audio)
