5017163795f9afca49a5f1edce1f31fedcf631de
Arneeth <157263299+arneethr@users.noreply.github.com>
2024-09-04 18:54:40 +0530

    creating a function for machine to speak

--- Dictionary using nltk/Dictionary.py
+++ Dictionary using nltk/Dictionary.py
@@ -3,3 +3,10 @@ from nltk.corpus import wordnet
 from tkinter import *
 
 nltk.download('wordnet')
+
+def speak(audio):
+    engine = pyttsx3.init(sapi5)
+    voices = engine.getProperty('voices')
+    voice = engine.setProperty('voice',voices[0].id)
+    engine.say(audio)
+    engine.runAndWait()
